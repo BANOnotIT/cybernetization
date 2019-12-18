d86485fbfd77d8c908dc9536fb8b6a081dd8330e
BANO.notIT <bano.notit@yandex.ru>
2019-12-18 19:59:00 +0300

    feat: add duration into quotes animation

--- src/js/quotes-animation.js
+++ src/js/quotes-animation.js
@@ -1,9 +1,10 @@
 const initQuotesAnimation = () => {
+    const duration = 1500;
+
     function animateBlock(elem) {
+        const started = Date.now();
         const originalText = elem.innerText;
 
-        // const CODE = "&#TфЕ3фПнфПШыпЫ.6^⅋¶4;";
-        // CODE.charAt(Math.floor(Math.random() * CODE.length))
         let randomText = originalText.replace(/\S/g, () => String.fromCharCode(0x20 + Math.floor(Math.random() * (0x24f - 0x20))));
         elem.innerText = randomText;
 
@@ -12,12 +13,25 @@ const initQuotesAnimation = () => {
             .map((_, i) => i)
             .sort(() => Math.random() - .5);
 
+
+        let lastProcessedIndex = 0;
+
         function an() {
-            const i = shuffledIndices.pop();
-            randomText = randomText.slice(0, i) + originalText[i] + randomText.slice(i + 1);
+            const d = (Date.now() - started) / duration;
+            const rightIndex = Math.ceil(d * randomText.length);
+
+            shuffledIndices
+                .slice(lastProcessedIndex, rightIndex)
+                .forEach(
+                    (i) =>
+                        randomText = randomText.slice(0, i) + originalText[i] + randomText.slice(i + 1)
+                );
+
             elem.innerText = randomText;
 
-            if (shuffledIndices.length)
+            lastProcessedIndex = rightIndex;
+
+            if (lastProcessedIndex < shuffledIndices.length)
                 requestAnimationFrame(an)
         }
 
